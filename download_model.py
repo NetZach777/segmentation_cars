abc0cf7e04ad7b26a45639d341d22f3f5b6256af
NetZach777 <150202716+NetZach777@users.noreply.github.com>
2024-10-12 03:16:28 +0200

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,9 +1,14 @@
 import boto3
+import os
 
 BUCKET_NAME = 'awsmodelseg'
 MODEL_KEY = 'unet_light_model_weighted_data_normal.h5'
-LOCAL_MODEL_PATH = '/tmp/unet_light_model_weighted_data_normal.h5'
+LOCAL_MODEL_PATH = '/home/ubuntu/unet_api/models/unet_light_model_weighted_data_normal.h5'
 
+# Vérifie si le répertoire existe, sinon crée-le
+os.makedirs(os.path.dirname(LOCAL_MODEL_PATH), exist_ok=True)
+
+# Télécharge le modèle depuis S3
 s3 = boto3.client('s3')
 s3.download_file(BUCKET_NAME, MODEL_KEY, LOCAL_MODEL_PATH)
 print(f"Model downloaded from S3: {MODEL_KEY}")
